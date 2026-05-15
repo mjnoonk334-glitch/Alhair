ded17e0208136fb75a37c6d8a7036a69f8e75881
ExoticN9 <mjnoonk334@gmail.com>
2026-05-15 17:46:48 +0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,3 +1,5 @@
+<!-- js/firebase.js -->
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
@@ -5,50 +7,36 @@ import {
   collection,
   addDoc,
   getDocs,
-  serverTimestamp
+  deleteDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+  orderBy,
+  query
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey: "ضع_apiKey",
-  authDomain: "ضع_authDomain",
-  projectId: "ضع_projectId",
-  storageBucket: "ضع_storageBucket",
-  messagingSenderId: "ضع_senderId",
-  appId: "ضع_appId"
-
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
-export async function saveReport(report){
-
-  try{
-
-    await addDoc(collection(db,"reports"),{
-      ...report,
-      createdAt:serverTimestamp()
-    });
-
-    return true;
-
-  }catch(error){
-
-    console.error(error);
-    return false;
-  }
-
-}
-
-export async function getReports(){
-
-  const snapshot = await getDocs(collection(db,"reports"));
-
-  return snapshot.docs.map(doc => ({
-    id:doc.id,
-    ...doc.data()
-  }));
-
-}
+export {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+  orderBy,
+  query
+};
+</script>
